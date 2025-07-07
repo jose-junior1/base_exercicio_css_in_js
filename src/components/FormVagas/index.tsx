589edc595fac270f66cbea76e4fbb782793a52be
José Junior <juhsantos.goleiro634@gmail.com>
2025-07-07 10:29:10 -0300

    Corrigindo erro de chamada homepage em package.json e criando styled components para cabeçalho e formulário

--- src/components/FormVagas/index.tsx
+++ src/components/FormVagas/index.tsx
@@ -1,6 +1,5 @@
 import { FormEvent, useState } from 'react'
-
-import styles from './FormVagas.module.css'
+import { Fml, BtnSearch, Campo } from '../Cabecalho/Cabecalho.module'
 
 type Props = {
   aoPesquisar: (termo: string) => void
@@ -15,17 +14,14 @@ const FormVagas = ({ aoPesquisar }: Props) => {
   }
 
   return (
-    <form className={styles.form} onSubmit={aoEnviarForm}>
-      <input
-        className={styles.campo}
+    <Fml>
+      <Campo
         placeholder="Front-end, fullstack, node, design"
         onChange={(e) => setTermo(e.target.value)}
         type="search"
       />
-      <button className={styles.btnPesquisar} type="submit">
-        Pesquisar
-      </button>
-    </form>
+      <BtnSearch type="submit">Pesquisar</BtnSearch>
+    </Fml>
   )
 }
 export default FormVagas
